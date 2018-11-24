5dafecdf769502bafd78b17735628c2a7ae0cd05
manishbupadhyay <ermanishupadhyay@gmail.com>
2018-11-24 23:51:56 +0530

    initial checkin

---
+++ 01Basics/variables.js
@@ -0,0 +1,14 @@
+var myFirstName = 'Manish';
+console.log(myFirstName);
+
+// Arrays
+var moviesList = ['Avatar','DDLJ','Transsformers'];
+console.log(moviesList);
+
+// Object
+var user = {
+    firstName: 'Manish',
+    surName: 'Upadhyay'
+};
+
+console.log(user.firstName);
