68681e0da211b01abcf0040d4cd091596f476733
manishbupadhyay <ermanishupadhyay@gmail.com>
2018-12-14 00:50:26 +0530

    template string

---
+++ ES6/templateStrings.js
@@ -0,0 +1,27 @@
+/* function employee (){
+    var name = "Manish";
+    var age = 27;
+    var position = "Developer";
+
+    //console.log("His name is "+ name +"  , and he is "+ age +" years old, he works as " + position); // ES5
+}
+
+employee(); */
+
+// ES6
+/* function employee (){
+    let name = "Manish";
+    let age = 27;
+    let position = "Developer";
+
+    console.log(`His name is  ${name} , and he is ${age} years old, he works as ${position}`); // ES6
+}
+
+employee(); */
+
+let name = "Bunty";
+let page = 7;
+
+const url = `http://www.bunty.com/${page}?name=${name}`;
+
+console.log(url);
